dab765e9470411ab5f76cd74963a3a7409e17b8e
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-08-17 11:25:45 +0530

    add new models

---
+++ src/main/java/model/Restaurant.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Restaurant {
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+}
