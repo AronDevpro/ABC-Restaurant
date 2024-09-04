49665b12b77f171d3e714917539f0b72c43293e4
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2024-09-04 18:22:34 +0530

    create email utility

---
+++ src/main/java/util/EmailUtil.java
@@ -0,0 +1,37 @@
+package util;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class EmailUtil {
+
+    public static void sendEmail(String recipientEmail, String subject, String messageContent, String email, String password) throws MessagingException {
+        // Configuring mail server properties
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        // Authenticating the session
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, password);
+            }
+        });
+
+        // Composing the message
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(email));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+        message.setSubject(subject);
+        message.setText(messageContent);
+
+        // Sending the email
+        Transport.send(message);
+    }
+}
